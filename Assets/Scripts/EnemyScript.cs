0b73068864bac3ed55fbbda3292939730007f5ce
Patrick-Mayer <53142589+Patrick-Mayer@users.noreply.github.com>
2024-05-24 01:51:06 -0400

    fixed some scripting issues

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -12,18 +12,25 @@ public class EnemyScript : MonoBehaviour
     protected int enemyHealth;
     protected float enemyMovementSpeed;
 
+    //the fact that I have to do it this way makes me think Unity's parents are cousins
+    protected Player playerScript;
+
+    private const int MAX_JANK_COOLDOWN = 60;
+    private int currentCooldown = 60;
+
     // Start is called before the first frame update
     void Start()
     {
-        
-    }    
+        playerScript = playerObj.GetComponent<Player>();
+    }
 
-    private void OnCollisionEnter(Collision collision)
+    private void OnTriggerEnter(Collider collision)
     {
         Debug.Log("collision detected");
-        
-        GameObject needleObj = GameObject.Find("Needle");     
-        if (collision.gameObject == needleObj) {            
+
+        GameObject needleObj = GameObject.Find("Needle");
+
+        if (collision.gameObject == needleObj) {
             enemyHealth--;
             enemySFX.Play();
             ScoreScript.AddScore(1);
@@ -31,16 +38,53 @@ public class EnemyScript : MonoBehaviour
             if(enemyHealth <= 0){
                 EnemyDeath();
             }
+
+        }else if(collision.gameObject == playerObj){
+            //playerScript.DamagePlayer(1);
+            //Debug.Log("we get here");
+            playerObj.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+            playerObj.GetComponent<Player>().DamagePlayer(1);
         }
+    }
+
+    //this is jank but it's a game jam
+    private void OnTriggerStay(Collider collision)
+    {
+        if (currentCooldown <= 0){
+            currentCooldown = MAX_JANK_COOLDOWN;
+
+            Debug.Log("collision detected");
+
+            GameObject needleObj = GameObject.Find("Needle");
+            if (collision.gameObject == needleObj) {
+                enemyHealth--;
+                enemySFX.Play();
+                ScoreScript.AddScore(1);
+
+                if(enemyHealth <= 0){
+                    EnemyDeath();
+                }
+
+            }else if(collision.gameObject == playerObj){
+                //playerScript.DamagePlayer(1);
+                //Debug.Log("we get here");
+                playerObj.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+                playerObj.GetComponent<Player>().DamagePlayer(1);
+            }
+
+        }else{
+            currentCooldown--;
+        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     void EnemyDeath(){
         Destroy(enemyObj);
     }
-}
+};
--- Assets/Scripts/TestScripts/Player.cs
+++ Assets/Scripts/TestScripts/Player.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
 
     public float speed = 5f; // Speed at which the object moves
     [SerializeField] private Rigidbody rb;
+
+    private int playerHealth = 3;
 
     // Define isometric movement vectors
     private Vector3 isometricRight = new Vector3(1, 0, 1).normalized;
@@ -17,14 +20,13 @@ public class Player : MonoBehaviour
     //this is all ChatGPT BS, but Unity's input system is stupid so I don't care.
     void Update()
     {
-        
+
         // Initialize a new velocity vector
         Vector3 newVelocity = Vector3.zero;
 
         // Check if any of the WASD keys are held down and update the velocity accordingly
         if (Input.GetKey(KeyCode.W))
         {
-            Debug.Log("try9ing to move up");
             newVelocity += isometricUp * speed;
         }
         if (Input.GetKey(KeyCode.A))
@@ -39,7 +41,17 @@ public class Player : MonoBehaviour
         {
             newVelocity += isometricRight * speed;
         }
-        
+
         rb.velocity = newVelocity;
+    }
+
+    public void DamagePlayer(int damage){
+        playerHealth -= damage;
+        Debug.Log(playerHealth);
+
+        if(playerHealth <= 0){
+            //reloads current level
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
     }
 }
